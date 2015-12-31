c8333c4616f4bb8c9d51c37b89e922b7790f547c
Carter Merenstein <cmerenstein@gmail.com>
2015-12-30 23:51:00 -0500

    fixed bug with fasta file in, back to speed

--- halign.cpp
+++ halign.cpp
@@ -44,10 +44,16 @@ that was bad.
 				not_first = true;
 			}
 			
-			string sequence = "";
+			sequence = "";
 		}
 	}
 	sequences.push_back(sequence); // put in the last one
+	
+	// int q;
+	// for (q=0;q<sequences.size();q++){
+		// cout << sequences[q] << endl;
+	// }
+	// return nullptr;
 	
 	int stride;			// used for skipping through sequences before trying to align all
 	if (sequences.size() < 12){ // play with these numbers more
@@ -68,7 +74,7 @@ that was bad.
 	
 	int a, b, c, d; // a loops through sequences, b thru sequence a, c thru all sequences, d thru sequence c
 	for (a = 0; a < sequences.size(); a++){
-		printf("\r%.0f%% aligned.", (float(a)/float(sequences.size()))*100);
+		// printf("\r%.0f%% aligned.", (float(a)/float(sequences.size()))*100);
 		string s_a = sequences[a];
 		for ( b = 0; (b + k) < s_a.size(); b++){
 			// if (b%100 == 0) {
